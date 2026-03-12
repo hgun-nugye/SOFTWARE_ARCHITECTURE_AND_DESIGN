a1ca3a4b2b5810da2df5be2cceacbb5a88ada8ec
Thanh Hương <thanhhuong10102005@gmail.com>
2026-03-12 10:42:53 +0700

    add Main FileFolder

---
+++ Exercises/src/thuchanh/B3_Composite/MainFile.java
@@ -0,0 +1,18 @@
+package thuchanh.B3_Composite;
+
+public class MainFile {
+    public static void main(String[] args) {
+        Folder folder1 = new Folder("Folder1", "2024-06-01");
+        Folder folder2 = new Folder("Folder2", "2024-06-02");
+
+        FileItem file = new FileItem("File1.txt", "2024-06-03");
+
+        folder1.addItem(folder2);
+        folder2.setParent(folder1);
+        folder2.addItem(file);
+        file.setParent(folder2);
+
+        System.out.println(folder1.getStringTreeFolder("|--"));
+        System.out.println(file.getPath());
+    }
+}
