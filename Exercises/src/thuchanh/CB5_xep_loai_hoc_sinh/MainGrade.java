4dc3689760b45b93e5da3d738cf8630cea6bfd1f
Thanh Hương <thanhhuong10102005@gmail.com>
2026-04-09 10:54:01 +0700

    add case main CB5 - Xep Loai Hoc Sinh

--- Exercises/src/thuchanh/CB5_xep_loai_hoc_sinh/MainGrade.java
+++ Exercises/src/thuchanh/CB5_xep_loai_hoc_sinh/MainGrade.java
@@ -3,17 +3,16 @@ package thuchanh.CB5_xep_loai_hoc_sinh;
 public class MainGrade {
     public static void main(String[] args) {
         GradeEvaluator yeu = new StandardEvaluator("Yếu", 0, 5, false);
-        GradeEvaluator tb = new StandardEvaluator("Trung bình", 5,7, false);
-        GradeEvaluator kha= new StandardEvaluator("Khá", 7, 8, false);
-        GradeEvaluator gioi= new StandardEvaluator("Giỏi", 8, 9, false);
-        GradeEvaluator xs= new StandardEvaluator("Xuất sắc", 9, 10, true);
+        GradeEvaluator tb = new StandardEvaluator("Trung bình", 5, 7, false);
+        GradeEvaluator kha = new StandardEvaluator("Khá", 7, 8, false);
+        GradeEvaluator gioi = new StandardEvaluator("Giỏi", 8, 9, false);
+        GradeEvaluator xs = new StandardEvaluator("Xuất sắc", 9, 10, true);
 
         yeu.setNextEvaluator(tb).setNextEvaluator(kha).setNextEvaluator(gioi).setNextEvaluator(xs);
 
-        double diem = 10.25;
-        yeu.evaluate(diem);
-
-        diem = 8.19;
-        yeu.evaluate(diem);
+        double[] diems = {4.5,4.999, 5.0, 6.5, 7.5, 8.5, 9.5, -1, 10.5};
+        for (double diem : diems) {
+            yeu.evaluate(diem);
+        }
     }
 }
